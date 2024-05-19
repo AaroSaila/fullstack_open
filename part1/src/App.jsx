73bbeda90acde420e83730e75bf8cf91ca608a46
Aaro Saila <aaros.saila@gmail.com>
2024-05-19 14:44:31 +0300

    progress

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,10 +1,36 @@
-const App = function() {
-  const friends = ["Peter", "Maya"]
+import { useState } from "react"
 
+const App = function () {
+  const [counter, setCounter] = useState(0)
+
+  function plusClick() {
+    console.log("clicked")
+    setCounter(counter + 1)
+  }
+
+  function minusClick() {
+    console.log("minus")
+    setCounter(counter -1)
+  }
+
+  function zeroClick() {
+    console.log("zeroed")
+    setCounter(0)
+  }
+  
   return (
-    <>
-      <p>{friends}</p>
-    </>
+    <div>
+      <div>{counter}</div>
+      <button onClick={plusClick}>
+        plus
+      </button>
+      <button onClick={minusClick}>
+        minus
+      </button>
+      <button onClick={zeroClick}>
+        zero
+      </button>
+    </div>
   )
 }
 
---
+++ test/test.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function bornYear() {return new Date().getFullYear() - 20};
+
+console.log(bornYear());

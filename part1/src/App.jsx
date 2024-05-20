a290d28743fb2905b14ea4e755175879243577dc
Aaro Saila <aaros.saila@gmail.com>
2024-05-20 14:21:01 +0300

    part1 progress

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from "react"
+import {useState} from "react"
 
-const App = function () {
-  const [counter, setCounter] = useState(0)
-
-  function plusClick() {
-    console.log("clicked")
-    setCounter(counter + 1)
+const History = function({allClicks}) {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
 
-  function minusClick() {
-    console.log("minus")
-    setCounter(counter -1)
-  }
-
-  function zeroClick() {
-    console.log("zeroed")
-    setCounter(0)
-  }
-  
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={plusClick}>
-        plus
-      </button>
-      <button onClick={minusClick}>
-        minus
-      </button>
-      <button onClick={zeroClick}>
-        zero
-      </button>
+      button press history: {allClicks.join(" ")}
+    </div>
+  )
+}
+
+const Button = function({handleClick, text}) {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = function() {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = function() {
+    setAll(allClicks.concat("L"))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = function() {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
